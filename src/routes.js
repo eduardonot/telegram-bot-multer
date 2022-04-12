c1c11fd32ac6cb755756103a7466fa9c1bbf76e9
eduardonot <eduardo_not@hotmail.com>
2022-04-12 16:00:10 -0300

    chore: add autoDelete field

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,9 @@ const Files = require('./models/Files')
 const telegram = require('./infra/telegramBot')
 
 routes.post('/upload', multer(multerConfig).single('file'), async (req, res) => {
+  const fourHours = 4 * 60 * 60 * 1000
+  const date = new Date()
+  const sumDate = date.getTime() + fourHours
   const { originalname: name, key, hash, size, location: url = '' } = req.file
   const sendFile = await Files.create({
     name,
@@ -12,6 +15,7 @@ routes.post('/upload', multer(multerConfig).single('file'), async (req, res) => 
     key,
     hash,
     url,
+    autoDeleteDate: new Date(sumDate),
     uploadCategory: req.body.category,
     sharingType: 'none',
     sellingValue: 0
@@ -25,7 +29,6 @@ routes.delete('/delete', async (req, res) => {
   const getFile = await Files.findOne({ hash: fileHash })
   await getFile.remove()
   res.status(200).send(getFile)
-  // const getFile = await Files.find
 })
 
 module.exports = routes
